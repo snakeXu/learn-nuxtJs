68c9b93ebfd9a798b112b6c2680aea6a8056e2ea
xujing <1239748054@qq.com>
2020-10-09 21:42:12 +0800

    04-base-nuxt-router

---
+++ pages/user/abc.vue
@@ -0,0 +1,27 @@
+/*
+ * @Author: xujing
+*/
+<template>
+  <div>
+    <h1>User abc page</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'UserAbcPage',
+  components: {
+  },
+  data () {
+    return {
+    }
+  },
+  created () {
+  },
+  methods: {
+  },
+  computed: {
+  }
+}
+</script>
+<style scoped>
+</style>
---
+++ pages/user/index.vue
@@ -0,0 +1,27 @@
+/*
+ * @Author: xujing
+*/
+<template>
+  <div>
+    <h1>User index page</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'UserPage',
+  components: {
+  },
+  data () {
+    return {
+    }
+  },
+  created () {
+  },
+  methods: {
+  },
+  computed: {
+  }
+}
+</script>
+<style scoped>
+</style>
