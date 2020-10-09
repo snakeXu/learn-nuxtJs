ba1f753fe0a42129666556cb785e60fcec73f17b
xujing <1239748054@qq.com>
2020-10-09 21:19:06 +0800

    first & init nuxt-demo

---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+/*
+ * @Author: xujing
+*/
+<template>
+  <div>
+    <h1>About page</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomePage',
+  components: {
+  },
+  data () {
+    return {
+    }
+  },
+  created () {
+  },
+  methods: {
+  },
+  computed: {
+  }
+}
+</script>
+<style scoped>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+/*
+ * @Author: xujing
+*/
+<template>
+  <div>
+    <h1>Hello Nuxt.js</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomePage',
+  components: {
+  },
+  data () {
+    return {
+    }
+  },
+  created () {
+  },
+  methods: {
+  },
+  computed: {
+  }
+}
+</script>
+<style scoped>
+</style>
